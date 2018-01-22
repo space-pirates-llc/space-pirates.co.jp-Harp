5c98b3ef88f699b78103e8e53e0ea0ae14e64d2e
出川　大和 <hxdegawa@gmail.com>
2018-01-22 18:09:09 +0900

    Added form name & property for netlify SES

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,7 @@
 $(function(){
+  
+//  DOM Functions
+  
   $("ul > li > a").on("click", function(){
     scrollTo($("." + $(this)[0].className.split("-")[0] + "-container"));
   });
@@ -16,5 +19,21 @@ $(function(){
   
   function scrollTo(locationElem){
     $("html,body").animate({scrollTop:locationElem.offset().top - 100});
+  };
+  
+//  Form Functions
+  
+  $('#recruit').submit(function(){
+    alert("ran");
+    return checkRecruitItems();
+  });
+  
+  function checkRecruitItems(){
+    for(var i = 0; i < $("#recruit > .form-set > input").length; i ++){
+      if($("#recruit > .form-set > input").eq(i).val().length = 0){
+        return false;
+      }
+    }
   }
+  
 })
