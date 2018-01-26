73454023dfc733f8197300a0e61d1c0e6ecf5731
出川　大和 <hxdegawa@gmail.com>
2018-01-26 12:17:54 +0900

    feat: Added form validation base

--- _source/javascript/index.js
+++ _source/javascript/index.js
@@ -1,39 +1,53 @@
 $(function(){
-  
+
 //  DOM Functions
-  
+
   $("ul > li > a").on("click", function(){
     scrollTo($("." + $(this)[0].className.split("-")[0] + "-container"));
   });
-  
+
   $(".btn-select-ship").on("click", function(){
     scrollTo($(".submit-card"));
     $(".submit-card, .minify-button").addClass("open");
     $(".shipname").eq(0).find("input").attr("value", $(this).parent().find(".card-header > span").text());
   });
-  
+
   $(".minify-button").on("click", function(){
     $(this).toggleClass("open");
     $(".submit-card").toggleClass("open");
   });
-  
+
   function scrollTo(locationElem){
     $("html,body").animate({scrollTop:locationElem.offset().top - 100});
   };
-  
+
 //  Form Functions
-  
-  $('#recruit').submit(function(){
-    alert("ran");
-    return checkRecruitItems();
+
+  $(".btn-submit-recruit").on("click", function(){
+    universalItemsJudge( $("#recruit").find("[type='text'], [type='email']") );
   });
-  
-  function checkRecruitItems(){
-    for(var i = 0; i < $("#recruit > .form-set > input").length; i ++){
-      if($("#recruit > .form-set > input").eq(i).val().length = 0){
+
+  $(".btn-submit-contact").on("click", function(){
+    universalItemsJudge( $("#contact").find("[type='text'], [type='email']") );
+  });
+
+  function universalItemsJudge( itemsJqElmArray ){
+    let errorStatus = {
+      msg: null,
+      targetJqElm: null
+    };
+    itemsJqElmArray.each(function (){
+      errorStatus.targetJqElm = $(this);
+      if( $(this).val().trim().length < 1 || $(this).val().trim().match(/^( |　|[0-9])$/) != null ){
+        errorStatus.msg = "item is empty.";
         return false;
       }
-    }
+      if( $(this).attr("type") == "email" && $(this).val().trim().match(/^[!#-9A-~]+@+[a-z0-9]+.+[^.]$/i) === null ){
+        errorStatus.msg = "item is not email.";
+        return false;
+      }
+    });
+    return errorStatus;
   }
-  
+
 })
